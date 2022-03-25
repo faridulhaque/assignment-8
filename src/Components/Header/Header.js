e2fc1b16a8d80d11d85a98ea475c3e4fa98e9902
faridulhaque <faridulhaquemurshed@gmail.com>
2022-03-25 22:46:53 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2 className=''>Best Products</h2>
+            <h4>Get Your Best Products From Us</h4>
+        </div>
+    );
+};
+
+export default Header;
